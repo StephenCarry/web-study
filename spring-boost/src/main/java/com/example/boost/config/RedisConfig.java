9823bcbda40408510474cba3b81a48c1d5fbd4b3
lumia <heyyaoo@qq.com>
2023-07-06 23:19:33 +0800

    update database to men mode

--- spring-boost/src/main/java/com/example/boost/config/RedisConfig.java
+++ spring-boost/src/main/java/com/example/boost/config/RedisConfig.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.redis.host:disable}")
+    @Value("${redis.host:disable}")
     private String host;
-    @Value("${spring.redis.port:0}")
+    @Value("${redis.port:0}")
     private int port;
-    @Value("${spring.redis.database:0}")
+    @Value("${redis.database:0}")
     private int database;
     @Bean
     public RedisUtil getRedisUtil() {
