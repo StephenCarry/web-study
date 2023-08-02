389891beee199c6c5d067151c7300ba889ad8a83
lumia <heyyaoo@qq.com>
2023-08-03 00:35:58 +0800

    logback update

--- spring-boost/src/main/java/com/example/boost/task/TestTask.java
+++ spring-boost/src/main/java/com/example/boost/task/TestTask.java
@@ -4,13 +4,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Component
 public class TestTask implements Runnable {
-
-    private static final ExecutorService threadPool = Executors.newFixedThreadPool(5);
 
     @Scheduled(fixedDelay = 3600*1000L)
     public void trigger() {
